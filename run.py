aad1796dfbdc0b4ff2ff5162faf056883ae18fe6
dimaszzz-benci <dimaszzz1601@gmail.com>
2026-03-18 01:57:13 +0700

    run.py

--- run.py
+++ run.py
@@ -1,9 +1,12 @@
-from app import create_app
+from app import create_app, db
+from app.models.user import User
+from app.models.film import Film
 
-# Buat app dari factory function
 app = create_app()
 
+# Inisialisasi database otomatis saat pertama deploy
+with app.app_context():
+    db.create_all()
+
 if __name__ == '__main__':
-    # Jalankan app di mode debug
-    # debug=True → otomatis reload kalau ada perubahan kode
     app.run(debug=True)
