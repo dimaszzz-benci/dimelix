614459876f8ef0b7c7e14644acde77c88295fb9f
dimaszzz-benci <dimaszzz1601@gmail.com>
2026-03-18 01:06:29 +0700

    run.py

--- run.py
+++ run.py
@@ -1,1 +1,9 @@
+from app import create_app
 
+# Buat app dari factory function
+app = create_app()
+
+if __name__ == '__main__':
+    # Jalankan app di mode debug
+    # debug=True → otomatis reload kalau ada perubahan kode
+    app.run(debug=True)
