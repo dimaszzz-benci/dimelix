0a2bbbb18a273444e317cf8ce2329245fc9161b4
dimaszzz-benci <dimaszzz1601@gmail.com>
2026-03-18 01:05:04 +0700

    __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,35 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
+from app.config import Config
 
+# Buat objek database, login manager, dan bcrypt
+# Dibuat di luar create_app agar bisa dipakai di file lain
+db = SQLAlchemy()
+login_manager = LoginManager()
+bcrypt = Bcrypt()
+
+def create_app():
+    # Buat Flask app
+    app = Flask(__name__)
+
+    # Masukkan konfigurasi dari config.py
+    app.config.from_object(Config)
+
+    # Hubungkan db, login_manager, bcrypt ke app
+    db.init_app(app)
+    login_manager.init_app(app)
+    bcrypt.init_app(app)
+
+    # Arahkan user ke halaman login kalau belum login
+    login_manager.login_view = 'auth.login'
+
+    # Daftarkan routes (nanti kita isi)
+    from app.routes.auth_routes import auth_bp
+    from app.routes.film_routes import film_bp
+
+    app.register_blueprint(auth_bp)
+    app.register_blueprint(film_bp)
+
+    return app
